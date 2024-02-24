54dd22ecaf257094124363e33fc1c853e848fb55
AI-Sherpa <agtgtsmq@duck.com>
2024-02-24 20:32:26 +0800

    Handle videos with no chapters.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='digestvid',
-    version='0.0.8',
+    version='0.0.9',
     author='AI Sherpa',
     author_email='contact@ai-sherpa.com',
     packages=find_packages(),
